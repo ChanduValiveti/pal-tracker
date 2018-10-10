047efbb0b99006ff604d58e86f92c6fe71e53cf3
agullipa@ford.com <agullipa@ford.com>
2018-10-10 14:38:33 -0400

    database migration lab

--- src/test/java/test/pivotal/pal/trackerapi/TimeEntryApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/TimeEntryApiTest.java
@@ -34,6 +34,7 @@ public class TimeEntryApiTest {
     public void testCreate() throws Exception {
         ResponseEntity<String> createResponse = restTemplate.postForEntity("/time-entries", timeEntry, String.class);
 
+
         assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
         DocumentContext createJson = parse(createResponse.getBody());
